ab02be109fafa916ea4ea579330c8aea54dea802
tlev159 <t.levente1598@gmail.com>
2021-06-25 15:57:43 +0200

    project init

---
+++ bikerrent/src/main/java/com/example/bikerrent/BikerrentApplication.java
@@ -0,0 +1,13 @@
+package com.example.bikerrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikerrentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikerrentApplication.class, args);
+    }
+
+}
---
+++ bikerrent/src/test/java/com/example/bikerrent/BikerrentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bikerrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikerrentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
