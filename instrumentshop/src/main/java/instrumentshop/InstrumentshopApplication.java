4cec1a5c2aaf37a0378c6373efc33bef574f7819
tlev159 <t.levente1598@gmail.com>
2021-07-03 09:57:04 +0200

    init instrument shop

---
+++ instrumentshop/src/main/java/instrumentshop/InstrumentshopApplication.java
@@ -0,0 +1,13 @@
+package instrumentshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstrumentshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstrumentshopApplication.class, args);
+    }
+
+}
---
+++ instrumentshop/src/test/java/instrumentshop/InstrumentshopApplicationTests.java
@@ -0,0 +1,13 @@
+package instrumentshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstrumentshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
